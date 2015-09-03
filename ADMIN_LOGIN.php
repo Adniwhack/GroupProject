a0b4d1844701ea615e40882b1a168d5eafd1b45c
Adniwhack <rasnayake.kavinda@gmail.com>
2015-09-04 00:19:06 +0530

    Updated UI of Admin

--- ADMIN_LOGIN.php
+++ ADMIN_LOGIN.php
@@ -13,78 +13,76 @@
             header("Location:admin_portal.php");
         }
         else{
-            echo "<script>alert('Username and password do not match')</script>";
+            echo "<script>alert('Username and passwords do not match')</script>";
         }
     }
     
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<!-- Adding bootstrap !-->
-<head>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
-</head>
-<body>
+    <!--  Adding bootstrap !-->
+  <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body background="images/234.jpg">
 
 <div class="container-fluid">
-<div class="row">
-<!-- Create the image view !-->
-<div class="col-md-12">
-<img alt="Bootstrap Image Preview" src="E:\Downloads\logo.png" class="center-block" />
-</div>
-<!-- Create the form which contains the name and password, md can be changed to sm etc !-->
-<div class="row">
-<div class="col-md-4" >
-</div>
-
-<div class="col-md-4">
-<!-- Create the form horizontally !-->
-<form class="form-horizontal" role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-
-<div class="form-group">
-<label for="email" class="col-sm-2 control-label">
-Email
-</label>
-
-<div class="col-sm-10">
-<input type="email" class="form-control" id="email" name="email"/>
-</div>
-</div>
-<!-- Input for the password !-->
-<div class="form-group"> 
-<label for="password" class="col-sm-2 control-label">
-Password
-</label>
-<div class="col-sm-10">
-<input type="password" class="form-control" id="password" name="password"/>
-</div>
-</div>
-<!-- Input for the check box !-->
-<div class="form-group">
-<div class="col-sm-offset-2 col-sm-10">
-<div class="checkbox">
-<label>
-<input type="checkbox" /> Remember me
-</label>
-</div>
-</div>
-</div>
-
-<div class="form-group">
-<div class="col-sm-offset-2 col-sm-10"> 
-<button type="submit" class="btn btn-default" >
-Login
-</button>
-</div>
-</div>
-
-</form>
-
-</div>
-</div>
-</div>
-</div>
-</div>
+	<div class="row">
+        <!--  Create the image view  !-->
+		<div class="col-md-12">
+                    <img alt="Bootstrap Image Preview" src="images/logo.png" class="center-block" />           
+		</div>
+        <!--  Create the form which contains the name and password, md can be changed to sm etc !-->
+			<div class="row">
+			<div class="col-md-4" >
+                        </div>	
+                <div class="col-md-4">
+                    <!--  Create the form horizontally !-->
+                    <form class="form-horizontal" role="form">
+					
+                        <div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">
+								Email
+							</label>
+							
+                            <div class="col-sm-10">
+								<input type="email" class="form-control" id="inputEmail3" />
+							</div>
+						</div>
+                        <!--  Input for the password !-->
+						<div class="form-group"> 
+							<label for="inputPassword3" class="col-sm-2 control-label">
+								Password
+							</label>
+							<div class="col-sm-10">
+								<input type="password" class="form-control" id="inputPassword3" />
+							</div>
+						</div>
+						<!--  Input for the check box !-->
+                        <div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<div class="checkbox">
+									<label>
+										<input type="checkbox" /> Remember me
+									</label>
+								</div>
+							</div>
+						</div>
+                        
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">		 
+								<button type="submit" class="btn btn-default">
+									Login
+								</button>
+							</div>
+						</div>
+                        
+					</form>
+				
+                </div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
